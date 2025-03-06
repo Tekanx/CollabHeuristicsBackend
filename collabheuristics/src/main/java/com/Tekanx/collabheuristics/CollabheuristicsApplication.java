cce1843c15066a9d194adeae6276e40652995b14
Roberto Isla <robertoisla92@gmail.com>
2025-03-06 12:45:52 -0300

    Init Backend Spring initiazr

---
+++ collabheuristics/src/main/java/com/Tekanx/collabheuristics/CollabheuristicsApplication.java
@@ -0,0 +1,13 @@
+package com.Tekanx.collabheuristics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollabheuristicsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollabheuristicsApplication.class, args);
+	}
+
+}
