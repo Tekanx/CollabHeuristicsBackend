9141a4df7e3eaee9fb59a74e90bd1049403715da
Roberto Isla <robertoisla92@gmail.com>
2025-03-12 11:15:03 -0300

    Actualización de Interface la cual extiende

--- collabheuristics/src/main/java/com/Tekanx/collabheuristics/repository/RepositorioCoordinador.java
+++ collabheuristics/src/main/java/com/Tekanx/collabheuristics/repository/RepositorioCoordinador.java
@@ -1,17 +1,18 @@
 package com.Tekanx.collabheuristics.repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
 /**
  * @author Tekanx
  * @version 0.1
  * @since 07/03/2025
  */
 
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import com.Tekanx.collabheuristics.models.Coordinador;
 
 @Repository
-public interface RepositorioCoordinador extends CrudRepository<Coordinador, Long> {
-
+public interface RepositorioCoordinador extends JpaRepository<Coordinador, Long> {
+    
 }
